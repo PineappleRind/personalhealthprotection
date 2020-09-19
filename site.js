7d94ea24d64d6532ee57a0af4d42bd71fdfb63e1
PineappleRind <pineapplerind.info@gmail.com>
2020-09-19 17:31:39 -0400

    site.js

---
+++ site.js
@@ -0,0 +1,36 @@
+      let modal = document.getElementById("modal");
+      let btn = document.getElementById("modalButton");
+      let span = document.getElementsByClassName("close")[0];
+      let modalContent = document.getElementById('modalContent');
+      
+      btn.onclick = function() {
+        modal.style.display = "block";
+        modalContent.style.animationName = "modal-fade-in";
+      }
+      
+      span.onclick = function() {
+        modal.style.display = "none";
+      }
+      
+      window.onclick = function(event) {
+        if (event.target == modal) {
+          modal.style.display = "none";
+        }
+      }
+    
+
+function handleSmallScreens() {
+  document.querySelector('.navbar-toggler')
+    .addEventListener('click', () => {
+    let navbarMenu = document.querySelector('.navbar-menu')
+
+    if (navbarMenu.style.display === 'flex') {
+      navbarMenu.style.display = 'none'
+      return
+    }
+
+    navbarMenu.style.display = 'flex'
+  })
+}
+
+handleSmallScreens()
