82e6a5091b2b9c4aecbdddfce61043279c28412d
northernlights03 <pineapplerind.info@gmail.com>
2020-10-03 21:12:37 -0400

    DIfferent nav

--- site.js
+++ site.js
@@ -17,21 +17,3 @@ window.onclick = function(event) {
     modal.style.display = "none";
   }
 }
-
-
-// Open links in mobiles
-function handleSmallScreens() {
-document.querySelector('.navbar-toggler')
-.addEventListener('click', () => {
-let navbarMenu = document.querySelector('.navbar-menu')
-
-if (navbarMenu.style.display === 'flex') {
-navbarMenu.style.display = 'none'
-return
-}
-
-navbarMenu.style.display = 'flex'
-})
-}
-
-handleSmallScreens()
